fa97bb15e35e60ae1fe21c707973f2e13daa27a4
Badri-narayan <badritulsiram@gmail.com>
2019-09-01 11:39:44 -0700

    removed iframe

--- script.js
+++ script.js
@@ -30,56 +30,10 @@ function generateProjects() {
     The Quiz App is an app developed by Phoebe Law and myself. It is to test the parents of
     bootcamp students on their knowledge on their child's bootcamp. <br>
     The Quiz App was made using: <br> JavaScript, CSS, HTML, and jQuery. <br>
+    <a id="github-repo" href="https://github.com/thinkful-ei-gecko/badri-phoebe-day-5" target="_blank"><img src="https://image.flaticon.com/icons/svg/25/25231.svg" style="width: 45px;" alt="link to github repo" target="_blank"></a>
+    <div id="link-desc">Link the the GitHub Repo</div>
     </p>
-    <div id="viewport" style="display:block; width: 45%;">
-        <div id="viewport-body" style="height: 500px;">
-        <iframe id="frame" src="https://thinkful-ei-gecko.github.io/badri-phoebe-day-5/" style="width: 300px; height: 500px; background-image: none; display: inline-block;">
-        #document
-        <html lang="en"><head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <meta http-equiv="X-UA-Compatible" content="ie=edge">
-            <link href="https://fonts.googleapis.com/css?family=Roboto&amp;display=swap" rel="stylesheet">
-            <link href="index.css" rel="stylesheet" type="text/css">
-            <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/4.1.1/normalize.min.css">
-            <title>How Much Do You Know About Your Child's Bootcamp?</title>
-            </head>
-            <body>
-            <header class="status-container">
-            <div><span class="score">Your Score: 0</span><span class="questionNum">Question: 1/5</span></div>
-            </header>
-            <main>
-            <div class="js-quiz-box"><div>
-            <h1 class="page-title">What is my child doing? </h1>
-            <form id="qForm" class="questionForm">
-            <fieldset class="answer-options">
-            <input type="radio" id="radio-1" class="input" name="quiz" value="Delaying adulthood" required="">  
-            <label class="answer-option" for="radio-1">Delaying adulthood</label>
-          
-            <input type="radio" id="radio-2" class="input" name="quiz" value="Learning how to use the Google" required="">
-            <label class="answer-option" for="radio-2">Learning how to use the Google</label>
-
-          <input type="radio" id="radio-3" class="input" name="quiz" value="Learning part time" required="">
-          <label class="answer-option" for="radio-3">Learning part time</label>    
-
-          <input type="radio" id="radio-4" class="input" name="quiz" value="Spending 40-60 hours weekly in an intensive program to become a junior developer in five months!" required="">
-          <label class="answer-option" for="radio-4">Spending 40-60 hours weekly in an intensive program to become a junior developer in five months!</label>
-
-          <div class="submit">
-            <button type="submit" class="submit-button">Submit</button>
-          </div>
-        </fieldset>
-        </form>
-        </div></div>
-        </main>
-        <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-        <script src="datastore.js"></script>
-        <script src="index.js"></script>
-
-        </body>
-        </html>
-        </iframe>
-    </div>
+    <a href="https://thinkful-ei-gecko.github.io/badri-phoebe-day-5/" target="_blank"><img src="https://i.imgur.com/oHQWbuU.png" alt="An Image of the quiz app" class="proj-img"></a>
     `);
 }
 
@@ -102,12 +56,12 @@ function generateContact(){
     <input type="reset" value="Reset" class="form-button">
     </form>
     <h2> You can also find me here: </h2>
-    <a href="https://linkedin.com/in/badritulsiram"><img src="https://image.flaticon.com/icons/svg/69/69406.svg" style="width: 50px;"></a>
-    <a href="https://twitter.com/badri_tulsiram"><img src="https://image.flaticon.com/icons/svg/69/69480.svg" style="width: 50px;"></a>
-    <a href="Https://badri2.tumblr.com"><img src="https://image.flaticon.com/icons/svg/69/69449.svg" style="width: 50px;"></a>
-    <a href="https://instagram.com/badri"><img src="https://image.flaticon.com/icons/svg/1384/1384015.svg" style="width: 50px;"></a>
-    <a href="https://github.com/Badri-narayan"><img src="https://image.flaticon.com/icons/svg/25/25231.svg" style="width: 50px;"></a>
-    <a href="https://facebook.com/Badri.Nar"><img src="https://image.flaticon.com/icons/svg/69/69407.svg" style="width: 50px;"></a>
+    <a href="https://linkedin.com/in/badritulsiram" target="_blank"><img src="https://image.flaticon.com/icons/svg/69/69406.svg" style="width: 50px;"></a>
+    <a href="https://twitter.com/badri_tulsiram" target="_blank"><img src="https://image.flaticon.com/icons/svg/69/69480.svg" style="width: 50px;"></a>
+    <a href="Https://badri2.tumblr.com" target="_blank"><img src="https://image.flaticon.com/icons/svg/69/69449.svg" style="width: 50px;"></a>
+    <a href="https://instagram.com/badri" target="_blank"><img src="https://image.flaticon.com/icons/svg/1384/1384015.svg" style="width: 50px;"></a>
+    <a href="https://github.com/Badri-narayan" target="_blank"><img src="https://image.flaticon.com/icons/svg/25/25231.svg" style="width: 50px;"></a>
+    <a href="https://facebook.com/Badri.Nar" target="_blank"><img src="https://image.flaticon.com/icons/svg/69/69407.svg" style="width: 50px;"></a>
     `);
 }
 
